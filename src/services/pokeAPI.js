5840d6f86798ea234b8cc4aebe0a1e538d1f1bfd
Jesus Salvador R <jesus.salvador.ro17@gmail.com>
2024-09-12 01:40:38 -0600

    Arregla lectura variables de ambiente

--- src/services/pokeAPI.js
+++ src/services/pokeAPI.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL;
+const API_URL = window._env_.REACT_APP_API_URL;
 
 export const fetchPokemonData = async (pokemonName) => {
   try {
