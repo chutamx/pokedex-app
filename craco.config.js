2544469b80f61948058336ccfe1c5b4edbf0cabb
Jesus Salvador R <jesus.salvador.ro17@gmail.com>
2025-02-04 20:43:15 -0600

    fix: actualizar dependencias y configuración de SVGO para resolver vulnerabilidades

--- craco.config.js
+++ craco.config.js
@@ -5,5 +5,40 @@ module.exports = {
     alias: {
       '@': path.resolve(__dirname, 'src'),
     },
+    configure: (webpackConfig) => {
+      // Encuentra la regla de SVGO
+      const svgRule = webpackConfig.module.rules.find(rule => 
+        rule.test && rule.test.toString().includes('svg')
+      );
+      
+      if (svgRule) {
+        // Modifica la configuración de SVGO para usar la versión más reciente
+        svgRule.use = svgRule.use.map(loader => {
+          if (loader.loader && loader.loader.includes('@svgr/webpack')) {
+            return {
+              ...loader,
+              options: {
+                ...loader.options,
+                svgoConfig: {
+                  plugins: [
+                    {
+                      name: 'preset-default',
+                      params: {
+                        overrides: {
+                          removeViewBox: false
+                        }
+                      }
+                    }
+                  ]
+                }
+              }
+            };
+          }
+          return loader;
+        });
+      }
+      
+      return webpackConfig;
+    }
   },
 };
